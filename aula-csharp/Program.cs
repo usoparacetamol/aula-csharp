1a7f76f3dfdd3eab921856b5df58d025d680e699
Murillo Aparecido <sirryuk1@gmail.com>
2026-05-26 23:10:43 -0300

    if e else em c#

--- aula-csharp/Program.cs
+++ aula-csharp/Program.cs
@@ -1,5 +1,11 @@
 ﻿double a = 10;
 double b = 20;
-bool iguais = a == b;
 
-Console.WriteLine(iguais);
+if(a == b)
+{
+    Console.WriteLine("São iguais");
+}
+else
+{
+    Console.WriteLine("São diferentes");
+}
